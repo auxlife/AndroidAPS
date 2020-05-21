a703639179b3521d415e3ceeb52d7785588e5642
auxlife <theju@auxlife.com>
2020-05-21 14:57:58 -0700

    Update SourceXdripPlugin.java

--- app/src/main/java/info/nightscout/androidaps/plugins/source/SourceXdripPlugin.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/source/SourceXdripPlugin.java
@@ -71,5 +71,6 @@ public class SourceXdripPlugin extends PluginBase implements BgSourceInterface {
 
     public void setSource(String source) {
         this.advancedFiltering = source.contains("G5 Native")||source.contains("G6 Native");
+        this.advancedFiltering = true;
     }
 }
